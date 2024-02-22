a5b865326d49d0b64d0ca423fdd0e4eafc0986d2
LorenzoGaetaniFinc <lorenzo.gaetani@finconsgroup.com>
2024-02-22 11:17:20 +0100

    create tables roles, users and users_roles

---
+++ src/main/java/com/fincons/taskmanager/entity/Role.java
@@ -0,0 +1,56 @@
+package com.fincons.taskmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy="roles")
+    private List<User> users;
+
+    @PreRemove
+    public void removeUsersAssociations() {
+        for (User user: this.users) {
+            user.getRoles().remove(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", users=" + users +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id == role.id && Objects.equals(name, role.name) && Objects.equals(users, role.users);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, users);
+    }
+}
