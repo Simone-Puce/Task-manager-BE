22f69f27d352699f944a643504579b096b8ac62e
LorenzoGaetaniFinc <lorenzo.gaetani@finconsgroup.com>
2024-03-07 11:46:52 +0100

    Implement create

--- src/main/java/com/fincons/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/fincons/taskmanager/repository/TaskRepository.java
@@ -3,6 +3,7 @@ package com.fincons.taskmanager.repository;
 import com.fincons.taskmanager.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,12 +14,4 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findAllByActiveTrue();
     boolean existsByTaskCode(String code);
     boolean existsByTaskCodeAndActiveTrue(String taskCode);
-    @Query(value = "SELECT AUTO_INCREMENT " +
-            "FROM information_schema.TABLES " +
-            "WHERE TABLE_SCHEMA = 'task-manager' " +
-            "AND TABLE_NAME = 'task'",
-            nativeQuery = true)
-    Long getNextSeriesId();
-    @Query("SELECT CONCAT(UPPER(t.taskName), '-', t.id) FROM Task t")
-    List<String> getTaskNameAndIdConcatenated();
 }
--- src/main/java/com/fincons/taskmanager/utility/NameValidator.java
+++ src/main/java/com/fincons/taskmanager/utility/NameValidator.java
@@ -10,7 +10,7 @@ public class NameValidator {
     public static String nameValidator(String inputText){
         boolean isMatch = Pattern.matches(NAME_REGEX_CONTAIN_LOWERCASE_AND_UPPERCASE_AND_NUMBER, inputText);
         if (isMatch){
-            String textWithoutDoubleSpace = replaceDoubleSpaces(inputText);
+            String textWithoutDoubleSpace = replaceDoubleOrMoreSpaces(inputText);
             String textWithoutSpaceBeforeAndAfter = replaceSpaceBeforeAndAfter(textWithoutDoubleSpace);
             boolean isMatch2 = Pattern.matches(NAME_REGEX_WITH_SPACE_IN_BUT_NOT_BEFORE_AND_AFTER_WITHIN_DOUBLE_SPACE, textWithoutSpaceBeforeAndAfter);
             if (isMatch2){
@@ -24,9 +24,8 @@ public class NameValidator {
             throw new IllegalArgumentException("Error: Name must only contain lowercase letters, uppercase letters, or numbers, without any special characters.");
         }
     }
-    private static String replaceDoubleSpaces(String input) {
-        Pattern pattern = Pattern.compile(" {2}");
-        return pattern.matcher(input).replaceAll(" ");
+    private static String replaceDoubleOrMoreSpaces(String input) {
+        return input.replaceAll("\\s+", " ");
     }
     private static String replaceSpaceBeforeAndAfter(String textWithoutDoubleSpace) {
         return textWithoutDoubleSpace.trim();
