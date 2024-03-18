9f0ddc52fc4ff433357e45739a55c9052735456e
LorenzoGaetaniFinc <152279548+LorenzoGaetaniFinc@users.noreply.github.com>
2024-03-18 11:20:41 +0100

    fix merge (#38)
    * added test entire controller and service for task, improve handle for mappings
    
    * resolve mapping user in board and task, merged dev into my branch
    
    * add password
    
    * remove useless getter and setter
    
    * implement findTaskByTaskCodeAndActiveTrue only in task service, start creating automatic code, handle control for the name in the taskController
    
    * Add query getTaskNameAndIdConcatenated
    
    * Changed password
    
    * Implement create
    
    * Remove useless properties
    
    * Remove code in all class, now using id for the query in db. Fix all put and create method in the single relationship.
    
    * Modify some message in controllers
    
    * Handle find for delete logic, now return in DTO only daughter active
    
    * Handle delete
    
    * Change relationship task-lane-board, and handle exception for query for DB
    
    * Implement logical delete in all classes
    
    * change password
    
    * Fix some mapper because merge dont work
    
    ---------
    
    Co-authored-by: Simone-Puce <152278228+Simone-Puce@users.noreply.github.com>

--- src/test/java/com/fincons/taskmanager/genericsTest/QueryTest.java
+++ src/test/java/com/fincons/taskmanager/genericsTest/QueryTest.java
@@ -16,30 +16,4 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 public class QueryTest {
 
-    @PersistenceContext
-    private EntityManager entityManager;
-    @Autowired
-    private BoardServiceImpl boardService;
-    @Autowired
-    private BoardRepository boardRepository;
-
-    @Test
-    public void queryTest(){
-        String jpql = "SELECT DISTINCT b " +
-                "FROM Board b " +
-                "LEFT JOIN FETCH b.tasks t " +
-                "LEFT JOIN FETCH b.boardsLanes bl " +
-                "LEFT JOIN FETCH bl.lane l " +
-                "WHERE b.boardId = :boardId AND b.active = true AND t.active = true AND bl.active = false AND l.active = true ";
-
-
-        Board board = entityManager.createQuery(jpql, Board.class)
-                .setParameter("boardId", 2)
-                .getSingleResult();
-
-        System.out.println(board.getTasks().size());
-        assertNotNull(board);
-        assertNotNull(board.getTasks());
-        assertFalse(board.getTasks().isEmpty());
-    }
 }
