8323875560d27b2e343640b554aca3da6cb263ce
Lorenzo Gaetani <lorenzo.gaetani.6@gmail.com>
2024-04-05 17:08:59 +0200

    add permission to Lane

--- src/main/java/com/fincons/taskmanager/service/laneService/LaneService.java
+++ src/main/java/com/fincons/taskmanager/service/laneService/LaneService.java
@@ -1,6 +1,7 @@
 package com.fincons.taskmanager.service.laneService;
 
 import com.fincons.taskmanager.entity.Lane;
+import com.fincons.taskmanager.exception.RoleException;
 
 import java.util.List;
 
@@ -8,7 +9,7 @@ public interface LaneService {
 
     Lane getLaneById(Long laneId);
     List<Lane> getAllLanes();
-    Lane createLane(Lane lane);
-    Lane updateLaneById(Long laneId, Lane lane);
-    void deleteLaneById(Long laneId);
+    Lane createLane(Lane lane) throws RoleException;
+    Lane updateLaneById(Long laneId, Lane lane) throws RoleException;
+    void deleteLaneById(Long laneId) throws RoleException;
 }
